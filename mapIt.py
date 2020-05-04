f260aa6a55c09eaef302bef7ccf34c8c2e38e9cb
cykz <cykz@cykz.com>
2020-05-04 20:58:35 +0200

    added pyperclip library and pyperclip.paste() method

--- mapIt.py
+++ mapIt.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
   address = ' '.join(sys.argv[1:]) #get address from command line
+
+else:
+  address = pyperclip.paste() #get address from clipboard
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
